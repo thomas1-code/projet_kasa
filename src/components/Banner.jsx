93d737c396fab17becb298acc3b35c51f442e44e
thomas1-code <thomas.o1@laposte.net>
2022-11-16 08:47:06 +0100

    création du composant Banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = ({ mobile, desktop, title, alt }) => {
+  return (
+    <div className="banner">
+      <picture>
+        <source media="(max-width: 991px)" srcSet={mobile} />
+        <source media="(min-width: 992px)" srcSet={desktop} />
+        <img className="banner__img" src={mobile} alt={alt} />
+      </picture>
+      {title ? <h2 className="banner__title">{title}</h2> : null}
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
+import BannerMobile from '../assets/img_banner_mobile.png';
+import BannerDesktop from '../assets/img_banner_desktop.png';
+
+const bannerTitle = 'Chez vous, partout et ailleurs';
+const bannerAlt = 'Paysage de falaises au bord de la mer';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner
+        mobile={BannerMobile}
+        desktop={BannerDesktop}
+        alt={bannerAlt}
+        title={bannerTitle}
+      />
+      <Footer />
     </div>
   );
 };

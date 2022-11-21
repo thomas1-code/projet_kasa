bff0852781c6a67a7b34ca8329a2c12e9f2226c2
thomas1-code <thomas.o1@laposte.net>
2022-11-21 12:47:35 +0100

    création de la page error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Error = () => {
   return (
     <div>
-      <h1>Erreur : 404</h1>
+      <Header />
+      <main className="error">
+        <h2 className="error__title">404</h2>
+        <p className="error__description">
+          Oups ! La page que <br /> vous demandez n'existe pas.
+        </p>
+        <Link to="/" className="error__link">
+          Retourner sur la page d'accueil
+        </Link>
+      </main>
     </div>
   );
 };

ead7f5ac6baaa4cb75c6179686ace78691b0aaec
thomas1-code <thomas.o1@laposte.net>
2022-11-24 15:23:37 +0100

    ajout d'une redirection en cas d'id incorrect en utilisant Navigate

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import Products from '../data/data-card.json';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -12,64 +12,65 @@ import Rating from '../components/Rating';
 const Accommodation = () => {
   const { id } = useParams();
   const product = Products.find((product) => product.id === id);
-  const {
-    pictures,
-    title,
-    location,
-    tags,
-    rating,
-    host,
-    description,
-    equipments,
-  } = product;
+
   return (
     <div>
-      <Header />
-      <main>
-        <Slideshow slides={pictures} alt={title} />
-        <section className="accommodation">
-          <div className="accommodation__container">
-            <div className="accommodation__header">
-              <h2 className="accommodation__title">{title}</h2>
-              <p className="accommodation__location">{location}</p>
-              <Tags>
-                <ul className="accommodation__tags">
-                  {tags.map((tag, index) => (
-                    <li className="accommodation__item" key={index}>
-                      {tag}
-                    </li>
-                  ))}
-                </ul>
-              </Tags>
-            </div>
-            <div className="accommodation__additionnal-info">
-              <Rating rating={rating} />
-              <Host name={host.name} image={host.picture} alt={host.name} />
-            </div>
-          </div>
-          <div className="accommodation__collapse">
-            <Collapse
-              label="Description"
-              className="accommodation__collapse-title"
-            >
-              <p className="accommodation__collapse-description">
-                {description}
-              </p>
-            </Collapse>
-            <Collapse
-              label="Équipements"
-              className="accommodation__collapse-title"
-            >
-              <ul className="accommodation__collapse-description">
-                {equipments.map((equipment, index) => (
-                  <li key={index}>{equipment}</li>
-                ))}
-              </ul>
-            </Collapse>
-          </div>
-        </section>
-      </main>
-      <Footer />
+      {product ? (
+        <div>
+          <Header />
+          <main>
+            <Slideshow slides={product.pictures} alt={product.title} />
+            <section className="accommodation">
+              <div className="accommodation__container">
+                <div className="accommodation__header">
+                  <h2 className="accommodation__title">{product.title}</h2>
+                  <p className="accommodation__location">{product.location}</p>
+                  <Tags>
+                    <ul className="accommodation__tags">
+                      {product.tags.map((tag, index) => (
+                        <li className="accommodation__item" key={index}>
+                          {tag}
+                        </li>
+                      ))}
+                    </ul>
+                  </Tags>
+                </div>
+                <div className="accommodation__additionnal-info">
+                  <Rating rating={product.rating} />
+                  <Host
+                    name={product.host.name}
+                    image={product.host.picture}
+                    alt={product.host.name}
+                  />
+                </div>
+              </div>
+              <div className="accommodation__collapse">
+                <Collapse
+                  label="Description"
+                  className="accommodation__collapse-title"
+                >
+                  <p className="accommodation__collapse-description">
+                    {product.description}
+                  </p>
+                </Collapse>
+                <Collapse
+                  label="Équipements"
+                  className="accommodation__collapse-title"
+                >
+                  <ul className="accommodation__collapse-description">
+                    {product.equipments.map((equipment, index) => (
+                      <li key={index}>{equipment}</li>
+                    ))}
+                  </ul>
+                </Collapse>
+              </div>
+            </section>
+          </main>
+          <Footer />
+        </div>
+      ) : (
+        <Navigate to="/error" />
+      )}
     </div>
   );
 };

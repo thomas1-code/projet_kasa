3de01f4a9731c43a67de3a9a0d70c2e23cf10c8b
thomas1-code <thomas.o1@laposte.net>
2022-11-24 10:37:24 +0100

    ajustement de margin et padding, suppression de arrow.png

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,10 +5,8 @@ import Banner from '../components/Banner';
 import Collapse from '../components/Collapse';
 import ImageMobile from '../assets/gustavo_alves.png';
 import ImageDesktop from '../assets/kalen_emsley.png';
-import Arrow from '../assets/arrow.png';
 
 const imageAlt = 'Paysage de montagnes enneigés';
-const symbolAlt = "Symbole d'un chevron";
 
 const About = () => {
   return (
@@ -22,28 +20,28 @@ const About = () => {
           alt={imageAlt}
         />
         <section className="about">
-          <Collapse label="Fiabilité" symbol={Arrow} alt={symbolAlt}>
+          <Collapse label="Fiabilité" className="about__title">
             <p className="about__description">
               Les annonces postées sur Kasa garantissent une fiabilité totale.
               Les photos sont conformes aux logements, et toutes les
               informations sont régulièrement vérifiées par nos équipes.
             </p>
           </Collapse>
-          <Collapse label="Respect" symbol={Arrow} alt={symbolAlt}>
+          <Collapse label="Respect" className="about__title">
             <p className="about__description">
               La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
               comportement discriminatoire ou de perturbation du voisinage
               entraînera une exclusion de notre plateforme.
             </p>
           </Collapse>
-          <Collapse label="Service" symbol={Arrow} alt={symbolAlt}>
+          <Collapse label="Service" className="about__title">
             <p className="about__description">
               Nos équipes se tiennent à votre disposition pour vous fournir une
               expérience parfaite. N'hésitez pas à nous contacter si vous avez
               la moindre question.
             </p>
           </Collapse>
-          <Collapse label="Sécurité" symbol={Arrow} alt={symbolAlt}>
+          <Collapse label="Sécurité" className="about__title">
             <p className="about__description">
               La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
               pour les voyageurs, chaque logement correspond aux critères de

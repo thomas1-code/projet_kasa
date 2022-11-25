390d3aece4a87818101d27c41bb1922a26547b19
thomas1-code <thomas.o1@laposte.net>
2022-11-25 13:49:29 +0100

    modification argument de fonction + <source> pour la version tablette

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Banner = ({ className, mobile, desktop, title, alt }) => {
+const Banner = ({ className, mobile, tablet, title, alt }) => {
   return (
     <div className={className}>
       <picture>
-        <source media="(max-width: 991px)" srcSet={mobile} />
-        <source media="(min-width: 992px)" srcSet={desktop} />
+        <source media="(max-width: 767px)" srcSet={mobile} />
+        <source media="(min-width: 768px)" srcSet={tablet} />
         <img className="banner__img" src={mobile} alt={alt} />
       </picture>
       {title ? <h2 className="banner__title">{title}</h2> : null}
